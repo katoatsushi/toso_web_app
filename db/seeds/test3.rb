1d33c78fac8b06ae1db499d273abd56fa3be6fc2
katoatsushi <your.email@example.com>
2018-12-24 22:46:16 +0900

    commit

---
+++ db/seeds/test3.rb
@@ -0,0 +1,4 @@
+Mainpart.create(mp_name: "屋根")
+Material.create(mate_name: "塗装(屋根)" ,mainpart_id: 3)
+Material.create(mate_name: "タイル(屋根)",mainpart_id: 3)
+Material.create(mate_name: "パネル(屋根)",mainpart_id: 3)
