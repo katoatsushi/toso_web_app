e77b4064ffd47954cd6c8c177b03ff0ffea92589
katoatsushi <your.email@example.com>
2018-12-24 23:29:13 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module SampleBuilding
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths += Dir[Rails.root.join('app', 'parameters', '**', '*')]
-
+    config.secret_key = '0e426eb2172ad5079619d0d0dcfea8362b83be53e19a0c96dc34f3a4abf401b7364dec0483780cbfe41d340865459cd7ba1ff4709d43df4f4e6cffc58be4920e'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
